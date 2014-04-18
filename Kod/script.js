68c7281bac6da4c7af7735e39307ebc1f4b91746
lo222fj <lo222fj@student.lnu.se>
2014-04-18 11:33:55 +0200

    Dokument för kod

---
+++ Kod/script.js
@@ -0,0 +1,14 @@
+"use strict";
+
+var cssChanger = {
+    
+    loadHtml: function() {
+        var codeHtml = "<DOCTYPE html>";
+    }
+    
+    
+}
+
+window.onload = function() {
+    cssChanger.loadHtml();
+}
