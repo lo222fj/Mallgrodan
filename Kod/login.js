1662058ac5960caa51896bf3a2cb81ce16991d74
lo222fj <lo222fj@student.lnu.se>
2014-05-03 09:52:34 +0200

    Lite mer kod...

--- Kod/login.js
+++ Kod/login.js
@@ -1,23 +1,28 @@
 ﻿var firebase = new Firebase('https://blinding-fire-8523.firebaseio.com/');
 var loginMessage = $('#loginMessage');
+var userEmail;
 
-
+//koppling till login-systemet
+//efter rad 5 är auth ett FirebaseSimpleLogin-objekt
 var auth = new FirebaseSimpleLogin(firebase, function (error, user) {
     console.log('i var auth');
+    console.log(auth + 'före if');
     if (error) {
         switch (error.code) {
             case 'INVALID_EMAIL': messages("Felaktig mailadress", "errorMessage")
             case 'EMAIL_TAKEN': messages("Angiven mailadress är upptagen", "errorMessage")
             case 'INVALID_USER': messages("Användaren finns inte hos Mallgrodan", "errorMessage")
-            case 'INVALID_PASSWORD': messages("Lösenordet stämmer inte med användaren")
+            case 'INVALID_PASSWORD': messages("Lösenordet stämmer inte med användaren", "errorMessage")
         }
         console.log(error);
     } else if (user) {
+        userEmail = user.email;
+        messages('Du är inloggad som '+ user.email, 'correctMessage');
         console.log('User ID: ' + user.uid + ', Provider: ' + user.provider);
 
     } else {
         console.log('Nope, no logged in user.');
-    }
+    } console.log(auth + 'sist i var aut');
 });
 
 function createUser() {
@@ -42,7 +47,7 @@ function createUser() {
 
 function login() {
     console.log('i login()')
-
+    
     auth.login("password", {
         email: email.value,
         password: password.value
