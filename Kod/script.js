cfd29301369baf16e44b2260557f1a4cccfd30b4
lo222fj <lo222fj@student.lnu.se>
2014-05-24 14:33:38 +0200

    Kod: Byte av layoutmall via drop-down fungerar

--- Kod/script.js
+++ Kod/script.js
@@ -4,114 +4,81 @@ var codeBody = "<div id=content>\n \t\t\t<p>Plupp</p>\n \t\t</div>";
 //var codeCss = "body {\n \tbackground-color: red;\n}";
 
 function htmlEncode(value) {
-    return $('<div/>').text(value).html();
+ return $('<div/>').text(value).html();
 }
 
 function loadHtml(codeHtml) {
-
  $('#html').append($('<pre></pre>'));
-    $('#html pre').append(htmlEncode(codeHtml));
+ $('#html pre').append(htmlEncode(codeHtml));
 }
 
 function loadCss(codeCss) {
-    $('#textareaCss').val(htmlEncode(codeCss));
+ $('#textareaCss').val(htmlEncode(codeCss));
 }
 
 function loadResult(codeHtml, codeCss) {
-    
-    var iframeResult = document.getElementById('iframeResult');
-    //iframeResult.srcdoc = codeHtml;
+ var iframeResult = document.getElementById('iframeResult');
+ //iframeResult.srcdoc = codeHtml;
 
-    setTimeout(function () {
-        iframeResult.contentDocument.head.appendChild(document.createElement("style"));
+ setTimeout(function () {
+  iframeResult.contentDocument.head.appendChild(document.createElement("style"));
 
-        var styleTag = iframeResult.contentDocument.getElementsByTagName('style');
-        styleTag[0].innerHTML = codeCss;
-    }, 300);
+  var styleTag = iframeResult.contentDocument.getElementsByTagName('style');
+  styleTag[0].innerHTML = codeCss;
+ }, 300);
 }
 
 function loadHtmlDoc() {
-    var xmlhttp;
-    if (window.XMLHttpRequest) {// code for IE7+, Firefox, Chrome, Opera, Safari
-        xmlhttp = new XMLHttpRequest();
-    } else {// code for IE6, IE5
-        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-    }
-    var codeHtml;
-    xmlhttp.onreadystatechange = function () {
-        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-            codeHtml = xmlhttp.responseText;
-            loadHtml(codeHtml);
-            
-        }
-    }
-    //Sista parametern i .open är ändrad från true (asynkron hämtning) till false för att resultatet
-    //ska ha kommit innan funktionen returnerar. Bör ses över. Kan ställa till problem
-    xmlhttp.open("GET", "HtmlPage.html", false);
-    xmlhttp.send();
+ var xmlhttp;
+ if (window.XMLHttpRequest) {// code for IE7+, Firefox, Chrome, Opera, Safari
+  xmlhttp = new XMLHttpRequest();
+ } else {// code for IE6, IE5
+  xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+ }
+ var codeHtml;
+ xmlhttp.onreadystatechange = function () {
+  if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+   codeHtml = xmlhttp.responseText;
+   loadHtml(codeHtml);
 
-    return codeHtml;
+  }
+ }
+ //Sista parametern i .open är ändrad från true (asynkron hämtning) till false för att resultatet
+ //ska ha kommit innan funktionen returnerar. Bör ses över. Kan ställa till problem
+ xmlhttp.open("GET", "HtmlPage.html", false);
+ xmlhttp.send();
+
+ return codeHtml;
 }
 
-//BORTKOMMATERAR FUNGERANDE KOD OCH SKRIVER NY FÖR TEST AV ATT LÄGGA IHOP TVÅ CSS-DOKUMENT
-//Läser in defaultmallen för css 
-//function loadCssDoc() {
-//    var xmlhttp;
+function getCssLayout(layoutStyleFile) {
+ var xmlhttp;
 
-//    if (window.XMLHttpRequest) {// code for IE7+, Firefox, Chrome, Opera, Safari
-//        xmlhttp = new XMLHttpRequest();
-//    } else {// code for IE6, IE5
-//        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-//    }
-
-//    var codeCss;
-//    xmlhttp.onreadystatechange = function () {
-//        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-//            codeCss = xmlhttp.responseText;
-//            loadCss(codeCss);
-//            //console.log("laddat css");
-//        }
-//    }
-//    //Sista parametern i .open är ändrad från true (asynkron hämtning) till false för att resultatet
-//    //ska ha kommit innan funktionen returnerar. Bör ses över. Kan ställa till problem
-//    xmlhttp.open("GET", "StyleDefault.css", false);
-//    //console.log("före sänd");
-//    xmlhttp.send();
-//    //console.log("Efter sänd");
-
-//    return codeCss;
-//}
- 
-function getCssLayout(StylesheetFile) {
-     var xmlhttp;
-
-     if (window.XMLHttpRequest) {// code for IE7+, Firefox, Chrome, Opera, Safari
-         xmlhttp = new XMLHttpRequest();
-     } else {// code for IE6, IE5
-         xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-     }
-
-     var codeLayout;
-     xmlhttp.onreadystatechange = function () {
-         if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-             codeLayout = xmlhttp.responseText;
-             //loadCss(codeLayout); flyttad till onload för att slå ihop båda
-             //console.log("laddat css");
-         }
-     }
-     //Sista parametern i .open är ändrad från true (asynkron hämtning) till false för att resultatet
-     //ska ha kommit innan funktionen returnerar. Bör ses över. Kan ställa till problem
-     xmlhttp.open("GET", StylesheetFile, false);
-     //xmlhttp.open("GET", "StyleLayoutSimple.css", false);
-     //console.log("före sänd");
-     xmlhttp.send();
-     //console.log("Efter sänd");
-
-     return codeLayout;
+ if (window.XMLHttpRequest) {// code for IE7+, Firefox, Chrome, Opera, Safari
+  xmlhttp = new XMLHttpRequest();
+ } else {// code for IE6, IE5
+  xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
  }
+ var codeLayout;
+ xmlhttp.onreadystatechange = function () {
+  if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+   codeLayout = xmlhttp.responseText;
+   //loadCss(codeLayout); flyttad till onload för att slå ihop båda
+   //console.log("laddat css");
+  }
+ }
+ //Sista parametern i .open är ändrad från true (asynkron hämtning) till false för att resultatet
+ //ska ha kommit innan funktionen returnerar. Bör ses över. Kan ställa till problem
+ xmlhttp.open("GET", layoutStyleFile, false);
+ //xmlhttp.open("GET", "StyleLayoutSimple.css", false);
+ //console.log("före sänd");
+ xmlhttp.send();
+ //console.log("Efter sänd");
 
-function getCssColor() {
- 
+ return codeLayout;
+}
+
+function getCssColor(codeColorFile) {
  var xmlhttp;
 
  if (window.XMLHttpRequest) {// code for IE7+, Firefox, Chrome, Opera, Safari
@@ -124,13 +91,11 @@ function getCssColor() {
  xmlhttp.onreadystatechange = function () {
   if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
    codeColor = xmlhttp.responseText;
-   //loadCss(codeColor); flyttad till onload för att slå ihop båda
-   //console.log("laddat css");
   }
  }
  //Sista parametern i .open är ändrad från true (asynkron hämtning) till false för att resultatet
  //ska ha kommit innan funktionen returnerar. Bör ses över. Kan ställa till problem
- xmlhttp.open("GET", "StyleDefaultColor.css", false);
+ xmlhttp.open("GET", codeColorFile, false);
  //console.log("före sänd");
  xmlhttp.send();
  //console.log("Efter sänd");
@@ -138,52 +103,57 @@ function getCssColor() {
  return codeColor;
 }
 
-window.onload = function () {
+function changeTheme() {
+ var layoutStyleFile = $('#layoutThemes').val();
+ var codeColorFile = $('#colorThemes').val();
+ 
+ var codeLayout = getCssLayout(layoutStyleFile);
+ var codeColor = getCssColor(codeColorFile);
+ var codeHtml = $('#html').val();
 
- //Bortkommenteras när jag testar nytt. Kan ta tillbaka som det är
- //var codeHtml = loadHtmlDoc();
- //   var codeCss = loadCssDoc();
- //   console.log(codeCss);
- //   loadResult(codeHtml, codeCss);
-
- var codeHtml = loadHtmlDoc();
- var codeLayout = getCssLayout("StyleLayoutTwo.css");
- console.log(codeLayout);
- var codeColor = getCssColor();
- console.log(codeColor);
  var codeCss = codeColor + codeLayout;
  loadCss(codeCss);
-     console.log(codeCss);
-    loadResult(codeHtml, codeCss);
 
-    $('#buttonUpdateCss').on('click', function () {
-        var codeCss = $('#textareaCss').val();
-        loadResult(codeHtml, codeCss);
-    });
-  
-    prepareLogin();
+ loadResult(codeHtml, codeCss);
+}
 
-    $('#saveTemplate').on('click', function () {
-     saveCssTemplateToFirebase();
-    });
-    $('#loadTemplate').on('click', function () {
-     loadCssTemplateFromFirebase();
-    });
-    $('#viewTemplates').on('click', function () {
-     viewSavedTemplates();
-    });
-    $('#remove').on('click', function () {
-     removeTemplate();
-    });
- //$('#loginButton').on('click', function() {
- //    login();
- //});
+window.onload = function () {
+ //Laddar rutorna för Html, Css och Resultat
+ var codeHtml = loadHtmlDoc();
+ var codeLayout = getCssLayout("StyleLayoutSimple.css");
+ //console.log(codeLayout);
+ var codeColor = getCssColor("StyleBlackWhiteColor.css");
+ //Sconsole.log(codeColor);
+ var codeCss = codeColor + codeLayout;
+ loadCss(codeCss);
+ //console.log(codeCss);
+ loadResult(codeHtml, codeCss);
 
- //$('#logoutButton').on('click', function() {
- //    logout(); 
- //});
+ $('#buttonUpdateCss').on('click', function () {
+  var codeCss = $('#textareaCss').val();
+  console.log(codeCss + 'i onload')
+  loadResult(codeHtml, codeCss);
+ });
 
- //$('#createUser').on('click', function () {
- //    createUser();
- //});
+ $('#layoutThemes').on('change', function () {
+  changeTheme();
+ });
+
+$('#colorThemes').on('change', function () {
+  changeTheme();
+ });
+
+ $('#saveTemplate').on('click', function () {
+  saveCssTemplateToFirebase();
+ });
+ $('#loadTemplate').on('click', function () {
+  loadCssTemplateFromFirebase();
+ });
+ $('#viewTemplates').on('click', function () {
+  viewSavedTemplates();
+ });
+ $('#remove').on('click', function () {
+  removeTemplate();
+ });
+ prepareLogin();
 }
